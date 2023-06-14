d326cbd430a8e9aa4f43ea3c7a31e6652b6fa60c
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-14 09:28:15 -0300

    feat: added user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,62 +1,65 @@
-import { Request, Response } from 'express'
-import { BadRequestError } from '../helpers/api-erros'
-import { userRepository } from '../repositories/userRepository'
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { BadRequestError, UnauthorizedError } from "../helpers/api-erros";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
+type JwtPayload = {
+	id: number
+}
 export class UserController {
-	async create(req: Request, res: Response) {
-		const { name, email, password } = req.body
+	async create(req: Request, res: Response){
+		const {name, email, password} =  req.body
 
-		const userExists = await userRepository.findOneBy({ email })
+		const userExists = await userRepository.findOneBy({email})
 
-		if (userExists) {
-			throw new BadRequestError('E-mail já existe')
+		if(userExists){
+			throw new BadRequestError('Email existente!')
 		}
 
-		const hashPassword = await bcrypt.hash(password, 10)
+		const hashPassword = await bcrypt.hash(password,10)
 
 		const newUser = userRepository.create({
-			name,
+			name, 
 			email,
-			password: hashPassword,
+			password: hashPassword
 		})
 
 		await userRepository.save(newUser)
 
-		const { password: _, ...user } = newUser
+		const {password: _, ...user} = newUser
 
 		return res.status(201).json(user)
 	}
 
-	async login(req: Request, res: Response) {
-		const { email, password } = req.body
+	async login(req: Request, res: Response){
+		const { email, password} = req.body
 
-		const user = await userRepository.findOneBy({ email })
+		const user = await userRepository.findOneBy({email})
 
-		if (!user) {
-			throw new BadRequestError('E-mail ou senha inválidos')
+		if(!user){
+			throw new BadRequestError('Email ou senha inválidos!')
 		}
 
-		const verifyPass = await bcrypt.compare(password, user.password)
+		const verifyPassword = await bcrypt.compare(password,user.password)
 
-		if (!verifyPass) {
-			throw new BadRequestError('E-mail ou senha inválidos')
+		if(!verifyPassword){
+			throw new BadRequestError('Email ou senha inválidos!')
 		}
 
-		const token = jwt.sign({ id: user.id }, process.env.JWT_PASS ?? '', {
-			expiresIn: '8h',
+		const token =  jwt.sign({id: user.id}, process.env.JWT_PASS ?? '', {
+			expiresIn: '1d'
 		})
 
-		const { password: _, ...userLogin } = user
+		const {password:_, ...userLogin} = user
 
 		return res.json({
 			user: userLogin,
-			token: token,
+			token: token
 		})
 	}
 
-	async getProfile(req: Request, res: Response) {
+	async getProfile(req: Request, res: Response){
 		return res.json(req.user)
 	}
 }
